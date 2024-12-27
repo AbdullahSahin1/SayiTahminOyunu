7f893214760c605ab5e7e1794001960b602da9ed
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-12-27 14:15:35 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, ImageBackground } from "react-native";
+import GameStartScreen from "./screens/GameStartScreen";
+import { LinearGradient } from "expo-linear-gradient";
+export default function App() {
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={["rgba(0,0,0,0.8)", "transparent"]}
+    >
+      <ImageBackground
+        style={styles.container}
+        source={require("./assets/back.jpg")}
+        imageStyle={styles.backImage}
+      >
+        <GameStartScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backImage: {
+    opacity: 0.5,
+  },
+});
---
+++ screens/GameStartScreen.js
@@ -0,0 +1,81 @@
+import { Alert, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import CustomButton from "../components/CustomButton";
+
+export default function GameStartScreen() {
+  const [enteredNumber, setEenteredNumber] = useState("");
+
+  function resetHandler() {
+    setEenteredNumber("");
+  }
+  function confirmHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Geçersiz Sayı!", "Sayı 1 ile 99 arasında olmalıdır", [
+        { text: "Tamam", style: "destructive", onPress: resetHandler },
+      ]);
+    }
+  }
+  function numberHandler(text) {
+    setEenteredNumber(text);
+  }
+  return (
+    <View style={styles.container}>
+      <Text>Sayı Tahmin Uygulaması</Text>
+      <View style={styles.card}>
+        <TextInput
+          style={styles.input}
+          keyboardType="number-pad"
+          maxLength={2}
+          onChangeText={numberHandler}
+          value={enteredNumber}
+        ></TextInput>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <CustomButton onPress={resetHandler}>Temizle</CustomButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <CustomButton onPress={confirmHandler}>Onayla</CustomButton>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    fiex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 50,
+  },
+  card: {
+    backgroundColor: "orange",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+    marginTop: 20,
+    borderRadius: 20,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 5,
+    shadowOpacity: 0.25,
+  },
+  input: {
+    borderBottomWidth: 2,
+    borderBottomColor: "yellow",
+    width: 50,
+    height: 50,
+    marginVertical: 10,
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
