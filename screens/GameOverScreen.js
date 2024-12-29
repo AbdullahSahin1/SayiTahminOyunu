d31a6cbf2582520dade73483165faab6ad4a7e9c
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-12-29 14:10:37 +0300

    Update GameOverScreen.js

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -3,8 +3,16 @@ import React from 'react';
 
 export default function GameOverScreen() {
   return (
-    <View>
-      <Text>GameOverScreen</Text>
+    <View style={styles.container}>
+      <Title>Oyun Tamamlandı</Title>
+      <View style={styles.imageView}>
+        <Image style={styles.image} source={require('../assets/Success.jpg')}></Image>
+      </View>
+      <Text style={styles.result}>
+        <Text style={styles.countAndNumber}>10</Text> denemeyle 
+        <Text style={styles.countAndNumber}> 50</Text> sayısını buldun
+      </Text>
+      <CustomButton>Yeni Oyuna Başla</CustomButton>
     </View>
   );
 }
